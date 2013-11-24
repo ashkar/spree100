4c1361877d6f5e57d3bd737504f0a4f74973e438
ashkar <ashkar55555@gmail.com>
2013-11-24 10:21:59 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spree100::Application.config.secret_token = 'd7bca4d0811d5360dcc88723f0f6555af91cda912c201fc15cd8b0731ef349895c4b509dde897336a01be63c3751c012a082b0d85caf682eb3f78a5eec9960eb'
