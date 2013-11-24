ca09faf6311464caae5e53042b5ac17b480a5ea5
ashkar <ashkar55555@gmail.com>
2013-11-24 10:54:27 +0530

    fancy

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131124044684) do
+ActiveRecord::Schema.define(:version => 20131124052322) do
 
   create_table "spree_activators", :force => true do |t|
     t.string   "description"
